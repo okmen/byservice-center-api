5df72eb27ae9d65943fbf9d2a1f17febc51d481e
zhangyao <zhangyao@chudaokeji.com>
2018-09-19 10:38:43 +0800

    cts用户模块

--- src/main/java/com/baiyue/services/cts/user/ICtsUserService.java
+++ src/main/java/com/baiyue/services/cts/user/ICtsUserService.java
@@ -1,5 +1,16 @@
 package com.baiyue.services.cts.user;
 
-public interface ICtsUserService {
+import com.utils_max.ResultMsg;
 
+public interface ICtsUserService {
+	/**
+	 * cts用户登陆（手机号/用户名登陆）
+	 * @author max
+	 * @date:   2018年9月19日
+	 * @Desc :
+	 * @param usernameOrPhone
+	 * @param pwd
+	 * @return
+	 */
+	public ResultMsg login(String usernameOrPhone,String pwd);
 }
